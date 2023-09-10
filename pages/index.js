9e20d06d92040c171538689d5bc99be6f7856d55
ShridiptaSatpati <shridipta2006@gmail.com>
2023-09-10 11:31:29 +0530

    changed alignments

--- pages/index.js
+++ pages/index.js
@@ -94,7 +94,7 @@ export default function Home() {
           <Socials className="mt-2 laptop:mt-5" />
         </div>
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0" ref={workRef}>
-          <h1 className="text-2xl text-bold">Work.</h1>
+          <h1 className="tablet:m-10 text-2xl text-bold">Work.</h1>
 
           <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
             {data.projects.map((project) => (
@@ -129,11 +129,11 @@ export default function Home() {
             </Link>
           </div>
         )}
-        <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
+        <div className="mt-10 laptop:mt-30 p-2 laptop:p-0" ref={aboutRef}>
           <h1 className="tablet:m-10 text-2xl text-bold">About.</h1>
           {/* <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5"> */}
-          <p className="tablet:m-10 mt-2 text-xl laptop:text-2xl w-full laptop:w-3/5">
-          {/* <p className="tablet:m-10 laptop:text-2xl "> */}
+          <p className="tablet:m-10 mt-2 opacity-60 text-2xl laptop:text-xl w-full laptop:w-4/5">
+            {/* <p className="tablet:m-10 laptop:text-2xl "> */}
             {data.aboutpara}
           </p>
         </div>
